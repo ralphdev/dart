7c32d263f7475d1605e7ac9db7da28244713fdde
Rafael Martinez <rafa.developers@gmail.com>
2020-11-20 17:09:08 -0500

    Final numbers

---
+++ bin/numbers.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // entry point for execution
+
+  //Variable = spot in memory. Or just a bucket to hold a value.
+  num age = 13;
+  double number = 23.34;
+
+  print(age);
+}
