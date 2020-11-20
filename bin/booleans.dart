01ba78d2ce0009db2d3154e32ba85e8de55ba441
Rafael Martinez <rafa.developers@gmail.com>
2020-11-20 18:19:10 -0500

    Final booleans

---
+++ bin/booleans.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  // entry point for execution
+
+  //Variable = spot in memory. Or just a bucket to hold a value.
+  bool isTrue = true;
+  bool isFalse = false;
+  print(isTrue);
+}
