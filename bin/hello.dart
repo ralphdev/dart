a9c1ebd67f32c0e62af01e6f27244ca0a5633b30
Rafael Martinez <rafa.developers@gmail.com>
2020-11-20 16:47:50 -0500

    Basic init

---
+++ bin/hello.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // entry point for execution
+
+  //Variable = spot in memory. Or just a bucket to hold a value.
+  var country = "Panama"; // string
+
+  print('Hello Rafa');
+  print(country);
+}
