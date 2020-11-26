4afb001a298489f4bac97d467b07958a45812788
Rafael Martinez <rafa.developers@gmail.com>
2020-11-25 21:06:56 -0500

    Final operator aritmethic

---
+++ bin/operator_aritmethic.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  // Operadores (-, +, *, %)
+  int number = 34;
+  int numberTwo = 2;
+
+  double pi = 3.14;
+  double gravity = 9.8;
+
+  var result = number + numberTwo;
+  var resultTwo = pi * gravity;
+
+  print("Resultado: $result, Resultado dos: $resultTwo");
+}
