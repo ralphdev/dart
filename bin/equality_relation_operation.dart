dda550926494a9608747ad3eedbd84722e8fa4f1
Rafael Martinez <rafa.developers@gmail.com>
2020-11-25 21:14:29 -0500

    Final operator equality and relational

---
+++ bin/equality_relation_operation.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // Operadores (-, +, *, %)
+  int number = 34;
+  int numberTwo = 2;
+
+  double pi = 3.14;
+  double gravity = 9.8;
+
+  print(pi <= gravity);
+
+  if (pi != gravity) {
+    print("Si es diferente");
+  } else {
+    print("No es diferente");
+  }
+}
